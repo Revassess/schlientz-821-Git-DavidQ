3853c696067333dade24af68b416b485f21b6bcd
David Quarles <David@davids-mbp.home>
2020-08-04 16:58:34 -0400

    message

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,7 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+	
+		return true;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,17 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		
+		int sum = 28;
+		
+		if(arr == null) {
+			return 0;
+		}
+		
+		for(int i = 2; i < arr.length; i++) {
+			sum = sum + 5;
+		}
+			
+		return sum;
 	}
 }
